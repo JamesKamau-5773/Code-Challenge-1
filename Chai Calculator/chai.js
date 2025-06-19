9108b949e474c0092183f1b8c4a76b3aa82c25b8
James Kamau <jamesnk5773@gmail.com>
2025-06-19 09:23:51 +0300

    debug chai.js

--- Chai Calculator/chai.js
+++ Chai Calculator/chai.js
@@ -8,18 +8,18 @@ const numberofCups = Number(prompt("How many cups of chai do you want to make?")
 // Define ingredient measurements per cup (in ml or teaspoons)
 const milkperCup = 50 ;// 50ml milk per cup
 const TeaLeavesperCup = 1 ; // 1 teaspoon per cup
-const sugarperCup = 2 ; // 2 teaspoons of sugar per cup
-
+const sugarpercup = 2 ; // 2 teaspoons of sugar per cup
+const waterpercup = 200;
 
 // Calculate total required ingredients
-const totalWater = numberofCups * waterperCup; 
+const totalWater = numberofCups * waterpercup; 
 const totalMilk = numberofCups * milkperCup;
 const totalteaLeaves = numberofCups * TeaLeavesperCup;
-const totalsugar = numberofCups * sugarperCup;
+const totalsugar = numberofCups * sugarpercup;
 
 // Display the results using  (backticks ` `)
   console.log(`for ${numberofCups} cup(s) of chai  you need:`);
   console.log(`- water: ${totalWater} ml`);
   console.log(`- milk: ${totalMilk} ml`);
   console.log(`- Tea Leaves (Majani): ${totalteaLeaves} teaspoon`);
-  console.log(`- Sugar (Sukari): ${totalSugar} teaspoon`);
+  console.log(`- Sugar (Sukari): ${totalsugar} teaspoon`);
